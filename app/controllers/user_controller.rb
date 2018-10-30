f7528fc0e8594be4673aae885fb77e30544098b8
pmouhali <p.m.272019@gmail.com>
2018-10-30 18:17:52 +0100

    form2ok

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,10 @@ class UserController < ApplicationController
   def create
     User.create(username:params[:username], email:params[:email], bio:params[:bio])
     redirect_to '/'
+
+
+
+
   end
 
 end
